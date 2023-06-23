104002c3548ba653d1b949e970b026692ae97332
asilxoja <asilxoja@gmail.com>
2023-06-23 16:32:39 +0500

    Roman to Integer

---
+++ main.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        Rum_nume = {
+         "I": 1,
+         'V': 5,
+         'X': 10,
+         'L': 50,
+         'C': 100,
+         'D': 500,
+         'M': 1000
+        }
+        string = s
+        ans = 0
+        for i in range(len(string)):
+            if i < len(string) - 1 and Rum_nume[string[i]] < Rum_nume[string[i+1]]:
+                ans -= Rum_nume[string[i]]
+            else:
+                ans += Rum_nume[string[i]]
+        return ans
